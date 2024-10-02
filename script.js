67a539b698665d15a88efb302c6a960e5f0693ec
StudentPM <studentpm@Administrators-MacBook-Pro.local>
2024-10-02 14:23:14 -0700

    Cleaned up again

--- script.js
+++ script.js
@@ -22,26 +22,21 @@ concat(numbers, OtherList);
 // FUNCTIONS -----------------
 
 function push(newNum){
-
     numbers[numbers.length] = newNum;
     console.log(numbers);
     return numbers;
         // Since the index is always -1, the next one will be the number that you pushed
-
 }
 
 function pop(list){
-
     var riddenNum = list[list.length-1];
     console.log(riddenNum);
-    //Returns the last index
-
+        //Returns the last index
     list.length--;
-    //Gets rid of the pushed in number now but it also affects the console log
+        //Gets rid of the pushed in number now but it also affects the console log
 }
 
 function toString(wordCalled){
-
     var calledWord = ""+wordCalled;
         //Puts it in the string
     console.log(calledWord);
@@ -49,37 +44,43 @@ function toString(wordCalled){
 }
 
 function join(array, separatorThing){
-
     var result ="";
-
+        //Empty variable
     for(var i=0; i<array.length;i++){
-
+        //For loop to continue putting the separator
         result+= array[i];
-
+        // Add the index to the result
         if (i !== array.length - 1){
             result += separatorThing;
                 //Continues onto the next thing and replaces the ", " into whatever the separator is
-        }
-            
+        }     
     }
-
     console.log(result);
+        //Proof
 }
 
 function concat(list1, list2){
-
+    //Puts together two arrays
     var concatList = [];
-    var amount = list1.length+list2.length;
-
+    var lastIndex = list2[list2.length-1];
+        //The variable
     for(var i=0; i<list1.length; i++){
-        concatList += (list1[i]+" ");
+        concatList += (list1[i]+", ");
         //Adds the List1 numbers
     }
-
     for(var i=0; i<list2.length; i++){
-        concatList += (list2[i]+" ");
-        //Adds the List2 numbers
+        //for loop for second list
+
+        if (list2[i]===list2[list2.length-1]){
+            concatList += list2[i];
+            //If its the last number, do not add a comma
+        }
+        else{
+            concatList += (list2[i]+", ");
+            //Adds the List2 numbers
+        }
+        
     }
-  
     console.log(concatList);
+        //Proof
 }
